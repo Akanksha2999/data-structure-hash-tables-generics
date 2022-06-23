3f0337b0dd6fccc45a573755d474fa799fd8ecb9
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-23 21:23:41 +0530

    ADDED: Frequency of words in sentence is counted and also printed

--- src/com/bridgelabz/CountWord.java
+++ src/com/bridgelabz/CountWord.java
@@ -3,10 +3,15 @@ package com.bridgelabz;
 import java.util.Hashtable;
 
 public class CountWord {
+
+    //Main method
     public static void main(String[] args) {
         Hashtable<String, Integer> hashtable = new Hashtable<>();
+
+        //A sentence of type string
         String str = "To be or not to be";
 
+        //Code to count the frequency of words in sentence
         String[] splitArray = str.toLowerCase().split(" ");
         for (int i = 0; i < splitArray.length; i++) {
             if (hashtable.containsKey(splitArray[i])) {
@@ -16,6 +21,8 @@ public class CountWord {
                 hashtable.put(splitArray[i], 1);
             }
         }
+
+        //Printing the number of frequency of words in sentence
         System.out.println("The count of each word in sentence is: " + hashtable);
     }
 }
