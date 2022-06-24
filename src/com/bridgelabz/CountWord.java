5ffd145011a82a6ae585c0cd9f86513423f79572
Akanksha2999 <akankshajadhav2999@gmail.com>
2022-06-24 16:31:04 +0530

    ADDED: Removed word avoidable from frequency counting of each word in a paragraph

--- src/com/bridgelabz/CountWord.java
+++ src/com/bridgelabz/CountWord.java
@@ -1,6 +1,7 @@
 package com.bridgelabz;
 
 import java.util.Hashtable;
+import java.util.Map;
 
 public class CountWord {
 
@@ -10,9 +11,12 @@ public class CountWord {
 
         //A paragraph of type string
         String str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+        str = str.replace("avoidable", " ");
+
+        //Converting all the uppercase word to lower case
+        String[] splitArray = str.toLowerCase().split(" ");
 
         //Code to count the frequency of words in paragraph
-        String[] splitArray = str.toLowerCase().split(" ");
         for (int i = 0; i < splitArray.length; i++) {
             if (hashtable.containsKey(splitArray[i])) {
                 int count = hashtable.get(splitArray[i]);
@@ -22,7 +26,9 @@ public class CountWord {
             }
         }
 
-        //Printing the number of frequency of words in paragraph
-        System.out.println("The count of each word in paragraph is: " + hashtable);
+        //Printing all frequency of words in paragraph
+        for (Map.Entry entry : hashtable.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
     }
 }
